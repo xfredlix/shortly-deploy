27dd8792f9baa6093453154c898a959c07a1bcd4
Hack Reactor Students <accounts@hackreactor.com>
2017-01-19 16:28:56 -0800

    should pass all tests locally

--- app/config.js
+++ app/config.js
@@ -1,17 +1,9 @@
-var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://198.199.105.231/var/shortly-deploy/db');
+mongoose.connect('mongodb://localhost:27017/testttttttt');
 
 var urlsSchema = new Schema({
   url: String,
@@ -22,11 +14,16 @@ var urlsSchema = new Schema({
   timestaps: {type: Date, default: Date.now}
 });
 
-urlsSchema.pre('save', function(next) {
+urlsSchema.post('init', function() {
   var shasum = crypto.createHash('sha1');
   shasum.update(this.url);
   this.code = shasum.digest('hex').slice(0, 5);
-  next();
+});
+
+urlsSchema.post('validate', function() {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
 });
 
 var Urls = mongoose.model('urls', urlsSchema);
@@ -37,24 +34,22 @@ var usersSchema = new Schema({
   timestaps: {type: Date, default: Date.now}
 });
 
-usersSchema.static.comparePassword = function(attemptedPassword, callback) {
+usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
     callback(isMatch);
   });
 };
 
-usersSchema.pre('save', function (next) {
+usersSchema.post('init', function () {
   var cipher = Promise.promisify(bcrypt.hash);
   cipher(this.password, null, null).bind(this)
     .then(function(hash) {
       this.password = hash;
-      next();
     });
 });
 
 var Users = mongoose.model('users', usersSchema);
 
-// console.log(mongoDB);
 module.exports.url = Urls;
 module.exports.user = Users;
 module.exports.urls = urlsSchema;
